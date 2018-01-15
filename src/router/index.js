a2b2269af942bef0de5991cc67e2d44677f8fd3d
Willie Chalmers III <willieechalmers@gmail.com>
2018-01-15 16:42:36 -0600

    Clean up routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import LeaderboardCard from '@/components/LeaderboardCard';
 import Contestants from '@/components/Contestants';
+import ProblemsList from '@/components/ProblemsList'
 
 Vue.use(Router);
 
@@ -9,7 +10,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Leaderboard',
+      name: 'Main',
       component: LeaderboardCard,
     },
     {
@@ -19,8 +20,13 @@ export default new Router({
     },
     {
       path: '/users',
-      name: 'Contestants',
+      name: 'Users',
       component: Contestants,
+    },
+    {
+      path: '/problems',
+      name: 'Problems',
+      component: ProblemsList,
     },
   ],
 });
