2cda9525596299055889225d47a305d78b9bcd80
Willie Chalmers III <willieechalmers@gmail.com>
2018-01-16 16:36:30 -0600

    Finalize App structure  - Add ton of styling  - Add tabs for rounds  - Add site footer

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,142 @@
 <template>
   <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-      <div class="tab-container" v-if="rounds.length > 0" v-for="round in rounds">
-        <div class="tab">
-          <div>ROUND {{ round.count }}</div>
+    <header class="toolbar">
+      <a href="/" class="logo"></a>
+      <span>Leaderboard <span class="beta-tag"><b>BETA</b></span>
+      </span>
+    </header>
+    <header v-if="true">
+      <div class="tab-container">
+        <div class="tab" v-for="round in rounds">
+          <span class="tab-content">
+            <router-link :to="'/round/' + round.count">ROUND {{ round.count }}</router-link>
+          </span>
         </div>
       </div>
     </header>
     <main>
       <router-view></router-view>
     </main>
+   <site-footer></site-footer>
   </div>
 </template>
 
 <script>
-import * as db from './scripts/database';
+import SiteFooter from "@/components/SiteFooter";
+import * as db from "./scripts/database";
 
 export default {
-  name: 'app',
-  computed: {
-    rounds: function () {
-      return db.fetchRounds();
-    },
+  name: "app",
+  components: {
+    "site-footer": SiteFooter
   },
-}
+  data() {
+    return {
+      
+    };
+  }
+};
 </script>
 
 <style>
 body {
   margin: 0;
+  background: #e0e0e0;
 }
 
 #app {
-  font-family: 'Roboto', 'Noto', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  font-family: "Roboto", "Noto", sans-serif;
+  color: #e0e0e0;
 }
 
 main {
   text-align: center;
   margin-top: 40px;
+  min-height: 70vh;
 }
 
 header {
   margin: 0;
   height: 56px;
   padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
+  background-color: #f5f5f5;
+  color: #000000;
+  opacity: 87%;
 }
 
 header span {
-  display: block;
-  position: relative;
+  display: inline-block;
   font-size: 20px;
   line-height: 1;
-  letter-spacing: .02em;
+  letter-spacing: 0.02em;
   font-weight: 400;
-  box-sizing: border-box;
+  vertical-align: middle;
+}
+
+header.toolbar {
   padding-top: 16px;
 }
 
-.tab-container {
-
+header:not(.toolbar) {
+  height: 48px;
 }
 
-div.tab {
+.logo {
+  vertical-align: middle;
+  display: inline-block;
+  width: 192px;
+  height: 24px;
+  background-image: url("~/static/img/logo_long.svg");
+  background-size: 192px 24px;
+}
+
+.beta-tag {
+  font-size: 12px;
+  display: inline;
+  color: #000000;
+}
+
+.tab-container {
+  display: flex;
+  align-items: flex-start;
+
   height: 48px;
-  padding: 8px;
   font-size: 14px;
+  font-weight: 500;
   overflow: hidden;
   user-select: none;
+
+  flex: 1 1 auto;
+  flex-direction: row;
 }
 
-div.tab[selected] {
+.tab {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1 1 auto;
+
+  position: relative;
+  padding: 0 8px;
+  overflow: hidden;
+  cursor: pointer;
+  vertical-align: middle;
+}
+
+.tab-content > * {
+  height: 100%;
+  transition: opacity 0.1s cubic-bezier(0.4, 0, 1, 1);
+  align-items: bottom;
+  justify-content: center;
+  flex: 1 1 auto;
+}
+
+.tab-content > a {
+  text-decoration: none;
+  color: #000000;
+}
+
+.tab-content[selected] > a {
+  color: #000000;
 }
 </style>
