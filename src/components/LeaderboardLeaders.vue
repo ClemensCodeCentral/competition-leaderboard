651df397df57bee37ce4365875e047a0581c2e40
Willie Chalmers III <willieechalmers@gmail.com>
2018-01-16 16:34:41 -0600

    Finalize LeaderboardLeaders structure

--- src/components/LeaderboardLeaders.vue
+++ src/components/LeaderboardLeaders.vue
@@ -1,29 +1,32 @@
 <template>
-  <div class="wrapper">
-    <span class="place">{{ place }}</span>
-    <img src="{{ imageSrc }}">
-    <div>{{ name }}</div>
-    <div>{{ points }}</div>
-  </div>
+  <section id="leaders">
+    <div class="wrapper" v-for="leader in leaders">
+      <img :src="leader.imageSrc" :alt="'Profile image for ' + leader.name">
+      <div class="place">{{ leader.place }}</div>
+      <div class="name-tag">{{ leader.name }}</div>
+      <div class="points-tag">{{ leader.points }}</div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   name: "leaderboard-leaders",
-  data() {
-    return {
-      name,
-      points,
-      place,
-      imageSrc
-    };
-  }
+  props: ["leaders"]
 };
 </script>
 
 <style scoped>
 img {
-  height: 32px;
-  width: 32px;
+  height: 128px;
+  width: 128px;
+}
+
+.name-tag {
+  font-size: 20px;
+}
+
+.points-tag {
+  font-size: 14px;
 }
 </style>
