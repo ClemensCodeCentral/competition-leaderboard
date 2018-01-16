723242c26b24b87e9b4efe852011909854af5bfe
Willie Chalmers III <willieechalmers@gmail.com>
2018-01-16 16:31:31 -0600

    Finalize Contestants component

--- src/components/Contestants.vue
+++ src/components/Contestants.vue
@@ -1,11 +1,27 @@
 <template>
-  
+  <div class="container">
+    <div class="contestant" v-for="contestant in contestants">
+      <span class="name">{{ contestant.name }}</span><span class="break"></span>
+      <span class="points">{{ contestant.points }}</span>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-  
-}
+  name: "leaderboard-contestants",
+  props: ["contestants"]
+};
 </script>
 <style scoped>
+div.contestant {
+  height: 72px;
+}
 
+div.contestant[alternate] {
+  background-color: #e0e0e0;
+}
+
+.break {
+  width: 16px;
+}
 </style>
